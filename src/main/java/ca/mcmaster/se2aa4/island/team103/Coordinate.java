bee5003beb341f38f7008d440349e629d9dff7b4
Matthew Nesbitt <mingsqu@gmail.com>
2024-02-07 16:05:23 -0500

    Completed Coordinate Class (Immutable)

--- src/main/java/ca/mcmaster/se2aa4/island/team103/Coordinate.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team103/Coordinate.java
@@ -1,5 +1,19 @@
 package ca.mcmaster.se2aa4.island.team103;
 
 public class Coordinate {
-	
+	private int x;
+	private int y;
+
+	public Coordinate(int x_in,int y_in) {
+		x = x_in;
+		y = y_in;
+	}
+
+	public int x() {
+		return x;
+	}
+
+	public int y() {
+		return y;
+	}
 }
