e3a7d300ab0e65df72ddf0be335651fcbc3384a6
Matthew Nesbitt <mingsqu@gmail.com>
2024-02-07 16:28:08 -0500

    Completed Turning Class

--- src/main/java/ca/mcmaster/se2aa4/island/team103/Navigation/Turning.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team103/Navigation/Turning.java
@@ -1,4 +1,5 @@
 package ca.mcmaster.se2aa4.island.team103.Navigation;
+import org.json.JSONObject;
 
 public class Turning {
 
@@ -9,12 +10,40 @@ public class Turning {
 		EAST
 	}
 
-	public void leftTurn(){
-
+	public JSONObject leftTurn(Direction current_heading){
+		// Returns JSONObject required for turning left - requires the current heading as argument
+		JSONObject leftTurn = new JSONObject();
+		JSONObject parameters = new JSONObject();
+		if(current_heading == Direction.NORTH) {
+			parameters.put("direction", "W");
+		} else if (current_heading == Direction.WEST) {
+			parameters.put("direction", "S");
+		} else if (current_heading == Direction.SOUTH) {
+			parameters.put("direction", "E");
+		} else if (current_heading == Direction.EAST) {
+			parameters.put("direction", "N");
+		}
+		leftTurn.put("action", "fly");
+		leftTurn.put("parameters", parameters);
+		return leftTurn;
 	}
 
-	public void rightTurn(){
-
+	public JSONObject rightTurn(Direction current_heading){
+		// Returns JSONObject required for turning right - requires the current heading as argument
+		JSONObject rightTurn = new JSONObject();
+		JSONObject parameters = new JSONObject();
+		if(current_heading == Direction.NORTH) {
+			parameters.put("direction", "E");
+		} else if (current_heading == Direction.WEST) {
+			parameters.put("direction", "N");
+		} else if (current_heading == Direction.SOUTH) {
+			parameters.put("direction", "W");
+		} else if (current_heading == Direction.EAST) {
+			parameters.put("direction", "S");
+		}
+		rightTurn.put("action", "fly");
+		rightTurn.put("parameters", parameters);
+		return rightTurn;
 	}
 
 	public void UTurn(){
