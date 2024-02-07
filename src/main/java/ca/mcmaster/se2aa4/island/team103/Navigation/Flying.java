7fbb36257d66b0b205fd7fb95073ae026a0e2bfc
Matthew Nesbitt <mingsqu@gmail.com>
2024-02-07 16:13:32 -0500

    Implemented Flying class

--- src/main/java/ca/mcmaster/se2aa4/island/team103/Navigation/Flying.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team103/Navigation/Flying.java
@@ -1,7 +1,10 @@
 package ca.mcmaster.se2aa4.island.team103.Navigation;
+import org.json.JSONObject;
 
 public class Flying {
-	public void flyForward(){
-		
+	public JSONObject flyForward(){
+		JSONObject fly = new JSONObject();
+		fly.put("action", "fly");
+		return fly;
 	}
 }
